fb2b4a96eef583943267a4dff47f877d24c00985
liu0912089608 <allen.liu@shinda.com.tw>
2021-03-08 14:11:37 +0800

    m

--- Carl/NCYU_practice_demo/page/shared.js
+++ Carl/NCYU_practice_demo/page/shared.js
@@ -1,7 +1,11 @@
 // 參考 https://codertw.com/%E5%89%8D%E7%AB%AF%E9%96%8B%E7%99%BC/286572/
+// 結果不能用 https://ithelp.ithome.com.tw/questions/10189068
 const ForReading = 1;
 let _ActiveXObject = new ActiveXObject('Scripting.FileSystemObject');
+console.log('_ActiveXObject', _ActiveXObject);
 let _TextFile = _ActiveXObject.OpenTextFile('./template.txt', ForReading);
+console.log('_TextFile', _TextFile);
 let _ReadLine = _TextFile.ReadLine();
-
+console.log('_ReadLine', _ReadLine);
 document.getElementById('template').innerHTML = _ReadLine;
+console.log('document.getElementById(template)', document.getElementById('template'));
